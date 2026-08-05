dfef743276db4ffd1e7dd1f319ab648995fa5937
Ajay Kapoor <ajaykapoor40@gmail.com>
2026-08-05 01:03:21 -0400

    Module-1: Big O Notations -Test Programs

---
+++ src/main/java/org/educative/module3/datastructures/linklists/CircularLinkedList.java
@@ -0,0 +1,516 @@
+package org.educative.module3.datastructures.linklists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class List3Node {
+    int data;
+    List3Node next;
+
+    public List3Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class CircleLinkedList {
+    List3Node head;
+
+    public CircleLinkedList() {
+        this.head = null;
+    }
+
+    public void insertAtEnd(int value) {
+        List3Node newNode = new List3Node(value);
+        if (this.head == null) {
+            newNode.next = newNode; // Point to itself to form a circular link
+            this.head = newNode;
+        } else  {
+            List3Node current = this.head;
+            while (current.next != this.head) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.next = this.head; // Complete the circular link
+        }
+    }
+
+    public void insertAtBeginning(int value) {
+        List3Node newNode = new List3Node(value);
+        if (this.head == null) {
+            newNode.next = this.head; // Point to itself to form a circular link
+            this.head = newNode;
+        } else  {
+            // go through to the end node
+            List3Node current = this.head;
+            while (current.next != this.head) {
+                current = current.next;
+            }
+            // here i am at end node
+            newNode.next = this.head;
+            current.next = newNode;
+            this.head = newNode;
+        }
+    }
+
+    public void insertAtPosition(int value, int position) {
+        if (position < 0) {
+            throw new IndexOutOfBoundsException("Negative positions are not allowed");
+        }
+
+        // Case 1: insert at beginning
+        if (position == 0) {
+            insertAtBeginning(value);
+            return;
+        }
+
+        // Case 2: insert somewhere after head
+        List3Node newNode = new List3Node(value);
+        List3Node current = head;
+
+        int index = 0;
+        // Traverse to position - 1
+        while (index < position - 1) {
+            current = current.next;
+            // If we loop back to head before reaching position - 1
+            if (current == head) {
+                throw new IndexOutOfBoundsException("Position is out of bounds");
+            }
+            index++;
+        }
+
+        // Insert newNode after current
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+
+    public void insertAfter(int target, int data) {
+        if (head == null) {
+            return;
+        }
+
+        List3Node current = head;
+
+        while (true) {
+            if (current.data == target) {
+                List3Node newNode = new List3Node(data);   // Create the new node
+                newNode.next = current.next;             // New node points to the next node
+                current.next = newNode;                  // Target node points to the new node
+                return;
+            }
+            current = current.next;
+            if (current == head) {                       // Target not found after one full cycle
+                break;
+            }
+        }
+    }
+
+    public boolean deleteFromBeginning() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        if (head.next == head) {
+            // single node list
+            head = null;
+            return true;
+        }
+        // traverse through such that you cycle back to start
+        List3Node current = head;
+        while (current.next != head) {
+            current = current.next;
+        }
+        head = head.next;
+        current.next = head;
+        return true;
+    }
+
+    public boolean deleteFromEnd() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        if (head.next == head) {
+            // single node list
+            head = null;
+            return true;
+        }
+        // traverse through such that you reach the 2nd last node
+        List3Node current = head;
+        while (current.next.next != head) {
+            current = current.next;
+        }
+        //at 2nd last node
+        current.next = head;
+        current.next.next = null;
+        return true;
+    }
+
+    public boolean deleteByValue(int value) {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        // If the head node matches, delete it by moving head forward
+        if (head.data == value) {
+            head = head.next;
+            return true;
+        }
+        List3Node current = head;
+        List3Node prev = null;
+        while (current != null && current.data != value) {
+            prev = current;
+            current = current.next;
+        }
+        if (current == null) {
+            return false;
+        }
+        prev.next = current.next;
+        return true;
+    }
+
+    public boolean deleteByPosition(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("Position must be non-negative");
+        }
+
+        // If the list is empty, nothing can be deleted
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+
+        // If the head node matches, delete it by moving head forward
+        if (position == 0) {
+            deleteFromBeginning();
+            return true;
+        }
+
+
+        int index = 0;
+        List3Node current = head;
+        while (current != null && index < position - 1) {
+            current = current.next;
+            index++;
+        }
+        // If position is out of range, do nothing
+        if (current == null || current.next == null) {
+            throw new IllegalArgumentException("Position out of range");
+        }
+        // Bypass the node at 'position'
+        current.next = current.next.next;
+        return true;
+    }
+
+    public void traverse() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        List3Node current = head;
+        while (true) {
+            System.out.print(current.data + " -> ");   // Process current node
+            current = current.next;                    // Move to the next node
+            if (current == head) {                     // Stop when traversal returns to head
+                break;
+            }
+        }
+//        do {
+//            System.out.print(current.data + " -> ");  // Example operation: print the value
+//            current = current.next;
+//        } while (current != head);
+        System.out.println(current.data + " -> back to head");  // Indicate the end of the list
+    }
+
+    public int count() {
+        if (head == null) {
+            return 0;
+        }
+        List3Node current = head;
+        int count =0;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    public boolean exists(int value) {
+        List3Node current = head;
+        while (current != null) {
+            if (current.data == value) {
+                return true;
+            };
+            current = current.next;
+        }
+        return false;
+    }
+
+    public int last() {
+        List3Node current = head;
+        if (current == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public List<Integer> toList() {
+        List<Integer> list = new ArrayList<>();
+        List3Node current = head;
+        while (current != null) {
+            list.add(current.data);
+            current = current.next;
+        }
+        return list;
+    }
+}
+
+//class CircleLinkedListWithTail {
+//    List3Node head;
+//    List3Node tail;
+//
+//    public CircleLinkedListWithTail() {
+//        this.head = null;
+//        this.tail = null;
+//    }
+//
+//    public void insertAtBegin(int value) {
+//        List3Node newNode = new List3Node(value);
+//        if (this.head == null) {
+//            this.head = newNode;
+//            this.tail = newNode;
+//        } else  {
+//            newNode.next = this.head;
+//            this.head = newNode;
+//        }
+//    }
+//
+//    public void insertAtEnd(int value) {
+//        List3Node newNode = new List3Node(value);
+//        if (this.head == null) {
+//            this.head = newNode;
+//            this.tail = newNode;
+//        } else  {
+//            this.tail.next = newNode;
+//            this.tail = newNode;
+//        }
+//    }
+//
+//    public void insertAtPosition(int value, int position) {
+//        if (position == 0) {
+//            insertAtBegin(value);
+//            return;
+//        }
+//        List3Node newNode = new List3Node(value);
+//        List3Node  current = this.head;
+//        int index = 0;
+//        while (current != null && index < position - 1) {
+//            current = current.next;
+//            index++;
+//        }
+//        if (current == null) {
+//            throw new IndexOutOfBoundsException("Position is out of bounds");
+//        }
+//        // Update links in the correct order
+//        newNode.next = current.next;
+//        current.next = newNode;
+//        if (current == this.tail) {
+//            tail.next = newNode;
+//            tail = newNode;
+//        }
+//    }
+//
+//    public void traverse() {
+//        List3Node current = head;
+//        while (current != null) {
+//            System.out.print(current.data + " -> ");  // Example operation: print the value
+//            current = current.next;
+//        }
+//        System.out.println("null");  // Indicate the end of the list
+//    }
+//
+//    public int count() {
+//        List3Node current = head;
+//        int count =0;
+//        while (current != null) {
+//            count++;
+//            current = current.next;
+//        }
+//        return count;
+//    }
+//
+//    public boolean exists(int value) {
+//        List3Node current = head;
+//        while (current != null) {
+//            if (current.data == value) {
+//                return true;
+//            };
+//            current = current.next;
+//        }
+//        return false;
+//    }
+//
+//    public int last() {
+//        List3Node current = head;
+//        if (current == null) {
+//            throw new IllegalStateException("List is empty");
+//        }
+//        while (current.next != null) {
+//            current = current.next;
+//        }
+//        return current.data;
+//    }
+//
+//    public List<Integer> toList() {
+//        List<Integer> list = new ArrayList<>();
+//        List3Node current = head;
+//        while (current != null) {
+//            list.add(current.data);
+//            current = current.next;
+//        }
+//        return list;
+//    }
+//
+//    public boolean deleteAtBegin() {
+//        if (head == null) {
+//            throw new IllegalStateException("List is empty");
+//        }
+//        head = head.next;
+//        if (head == null) { // If the list becomes empty, update tail as well
+//            tail = null;
+//        }
+//        return true;
+//    }
+//
+//    public boolean deleteByValue(int value) {
+//        if (head == null) {
+//            throw new IllegalStateException("List is empty");
+//        }
+//        // If the head node matches, delete it by moving head forward
+//        if (head.data == value) {
+//            head = head.next;
+//            if (head == null) { // If the list becomes empty, update tail as well
+//                tail = null;
+//            }
+//            return true;
+//        }
+//        List3Node current = head;
+//        List3Node prev = null;
+//        while (current != null && current.data != value) {
+//            prev = current;
+//            current = current.next;
+//        }
+//        if (current == null) {
+//            return false;
+//        }
+//        prev.next = current.next;
+//        if (current.next == null) { // If the deleted node was the tail, update tail
+//            tail = prev;
+//        }
+//        return true;
+//    }
+//
+//    public boolean deleteByPosition(int position) {
+//        if (position < 0) {
+//            throw new IllegalArgumentException("Position must be non-negative");
+//        }
+//
+//        // If the list is empty, nothing can be deleted
+//        if (head == null) {
+//            throw new IllegalStateException("List is empty");
+//        }
+//
+//        // If the head node matches, delete it by moving head forward
+//        if (position == 0) {
+//            deleteAtBegin();
+//            return true;
+//        }
+//
+//
+//        int index = 0;
+//        List3Node current = head;
+//        while (current != null && index < position - 1) {
+//            current = current.next;
+//            index++;
+//        }
+//        // If position is out of range, do nothing
+//        if (current == null || current.next == null) {
+//            throw new IllegalArgumentException("Position out of range");
+//        }
+//        // Bypass the node at 'position'
+//        current.next = current.next.next;
+//        if (current.next == null) { // If the deleted node was the tail, update tail
+//            tail = current;
+//        }
+//        return true;
+//    }
+//
+//
+//}
+
+public class CircularLinkedList {
+    public static void main(String[] args) {
+        // Create a linked list object
+        CircleLinkedList sampleList = new CircleLinkedList();
+
+        // Create nodes
+        List3Node first = new List3Node(10);
+        List3Node second = new List3Node(20);
+        List3Node third = new List3Node(30);
+
+
+        // Connect nodes
+        sampleList.head = first;
+        first.next = second;
+        second.next = third;
+
+        // Make the list circular by connecting the last node back to the first
+        third.next = sampleList.head;
+
+        // Print values
+        System.out.println("Head node value: " + sampleList.head.data);
+        System.out.println("Second node value: " + sampleList.head.next.data);
+        System.out.println("Third node value: " + sampleList.head.next.next.data);
+        System.out.println("-".repeat(100));
+
+        // Show the circular connection
+        System.out.println("\nNode after third: " + sampleList.head.next.next.next.data);
+        System.out.println("-".repeat(100));
+
+        // Verify the loop more clearly
+        System.out.println("\nDoes third.next point back to first? " + (third.next == sampleList.head));
+
+        // traversal
+        System.out.print("Traversal of circular linked list: ");
+        sampleList.traverse();
+        CircleLinkedList sampleList2 = new CircleLinkedList();
+
+        System.out.println("Creating a circular linked list by inserting 10, 20, 30, and 40 at the end:");
+        sampleList2.insertAtEnd(10);
+        sampleList2.insertAtEnd(20);
+        sampleList2.insertAtEnd(30);
+        sampleList2.insertAtEnd(40);
+        System.out.println("Current list:");
+        sampleList2.traverse();
+
+        System.out.println("\nInserting 5 at the beginning:");
+        sampleList2.insertAtBeginning(5);
+        System.out.println("List after inserting at the beginning:");
+        sampleList2.traverse();
+
+        System.out.println("\nDeleting the first node:");
+        sampleList2.deleteFromBeginning();
+        System.out.println("List after deleting from the beginning:");
+        sampleList2.traverse();
+
+        System.out.println("\nDeleting the last node:");
+        sampleList2.deleteFromEnd();
+        System.out.println("List after deleting from the end:");
+        sampleList2.traverse();
+
+        System.out.println("\nDeleting the node with value 20:");
+        sampleList2.deleteByValue(20);
+        System.out.println("List after deleting value 20:");
+        sampleList2.traverse();
+    }
+}
+
